84b5f0bae47a37e47847a192d373466073cee9cc
Daniel Afonso <danieljcafonso@gmail.com>
2024-05-27 22:56:08 +0100

    06_add_cache_action

--- src/lib/cars.ts
+++ src/lib/cars.ts
@@ -1,24 +1,26 @@
-"use server";
+import { action, cache } from "@solidjs/router";
 import { storage, Car } from "./db";
 
-export const getCars = async () => {
+export const getCars = cache(async () => {
+  "use server";
   return ((await storage.getItem("cars:data")) as Car[]).reverse();
-};
+}, "cars");
 
-export const getCar = async (id: number) => {
+export const getCar = cache(async (id: number) => {
+  "use server";
   return ((await storage.getItem("cars:data")) as Car[]).find(
     (car) => car.id === id
   );
-};
+}, "car");
 
-type CarInput = Pick<Car, "brand" | "model" | "description" | "price">;
-export const addCar = async (carInput: CarInput) => {
-  // const carInput = {
-  //   brand: String(data.get("brand")),
-  //   model: String(data.get("model")),
-  //   description: String(data.get("description")),
-  //   price: Number(data.get("price")),
-  // };
+export const addCar = action(async (data: FormData) => {
+  "use server";
+  const carInput = {
+    brand: String(data.get("brand")),
+    model: String(data.get("model")),
+    description: String(data.get("description")),
+    price: Number(data.get("price")),
+  };
 
   let [{ value: cars }, { value: index }] = await storage.getItems([
     "cars:data",
@@ -34,4 +36,4 @@ export const addCar = async (carInput: CarInput) => {
     storage.setItem("cars:counter", (index as number) + 1),
   ]);
   return car;
-};
+});
